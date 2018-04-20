e6394c6b8855744245491374a8992ac43820ad91
rose99hana <rose99hana@gmail.com>
2018-04-20 08:07:31 +0700

    upload to github

---
+++ JavaScript.js
@@ -0,0 +1,25 @@
+﻿window.onload = function () {
+    startSlides();
+};
+
+function startSlides() {
+    var slider, slidesNumber, slides, width, indexSlide, x;
+    indexSlide = 0;
+    width = 0;
+    slider = document.getElementById("slider");
+    slides = document.getElementsByClassName("slide");
+    x = slider.querySelectorAll("img");
+    slidesNumber = slides.length;
+    setInterval(function () {
+        if (indexSlide == (slidesNumber)) {
+            indexSlide = 0;
+            slider.style.marginLeft = "0px";
+            width = 0;
+        }
+        slider.style.transition = "1s ease";
+        slider.style.marginLeft = width + "px";
+        width = width - x[indexSlide].width;
+        indexSlide++;
+    }, 2000);
+}
+
